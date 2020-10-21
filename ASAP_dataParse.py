c20b7ca08d270ded2b5e9d145bd8357d07191aeb
Swapnil Parekh <swapnilbp100@gmail.com>
2020-10-22 04:17:39 +0530

    Update ASAP_dataParse.py

--- ASAP_dataParse.py
+++ ASAP_dataParse.py
@@ -113,7 +113,7 @@ def data(path, sets, path_o, folders = ['fold_0', 'fold_1','fold_2','fold_3', 'f
         test_str = os.path.join(f_p,'test.tsv')
         train_str = os.path.join(f_p,'train.tsv')
         dev_str = os.path.join(f_p,'dev.tsv')
-        names = ['essay_no','essay_id','essay','rater1_domain1','rater2_domain1','rater3_domain1','domain1_score',\
+        names = ['essay_id','essay_set','essay','rater1_domain1','rater2_domain1','rater3_domain1','domain1_score',\
         'rater1_domain2','rater2_domain2','domain2_score','rater1_trait1','rater1_trait2','rater1_trait3','rater1_trait4'\
         'rater1_trait5','rater1_trait6','rater2_trait1','rater2_trait2','rater2_trait3','rater2_trait4','rater2_trait5',\
         'rater2_trait6','rater3_trait1','rater3_trait2','rater3_trait3','rater3_trait4','rater3_trait5','rater3_trait6']
